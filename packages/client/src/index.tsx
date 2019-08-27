0144f274a6580e026c989fc0824f90f531e33052
csp89 <cspeck89+github@gmail.com>
2019-08-27 07:33:57 +0200

    add apollo client

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,10 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import { split } from "apollo-link";
+import { HttpLink } from "apollo-link-http";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const GRAPHQL_ENDPOINT = "192.168.0.220:3001/graphql";
+
+// Create an http link:
+const httpLink = new HttpLink({
+  uri: "http://" + GRAPHQL_ENDPOINT // use https for secure endpoint
+});
+
+// Create a WebSocket link:
+const wsLink = new WebSocketLink({
+  uri: "ws://" + GRAPHQL_ENDPOINT, // use wss for a secure endpoint
+  options: {
+    reconnect: true
+  }
+});
+
+// using the ability to split links, you can send data to each link
+// depending on what kind of operation is being sent
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const { kind } = getMainDefinition(query);
+    return kind === "OperationDefinition";
+  },
+  wsLink,
+  httpLink
+);
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
